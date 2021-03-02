86530e44253ec6e16b7db57723db38a0bb3e048a
CoaPro <srbpro18@gmail.com>
2021-03-02 21:06:13 +0100

    Begins

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://aleksandarlukovic.herokuapp.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aleksandar Luković | 1. 3. 2021. 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
