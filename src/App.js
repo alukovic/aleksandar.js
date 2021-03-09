945576e14900dbf270cd3eb10c050c3543e01a0b
CoaPro <srbpro18@gmail.com>
2021-03-09 19:07:49 +0100

    Update 1 | 9. 3. 2021.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
+import aca1 from './aca1.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={aca1} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://aleksandarlukovic.herokuapp.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aleksandar Luković | 1. 3. 2021. 
+          Aleksandar Luković
+          
         </a>
+
+    <ul className="ulApp1">
+        <li>Naziv sajta: Aleksandar-JS </li>
+        <li>Programski jezik: JavaScript</li>
+        <li>Front-end: React.js</li>
+        <li>Back-end: Node.js</li>
+        <li>Okvirna tema master rada: Sajber bezbednost u JavaScript Veb aplikacijama</li>
+    </ul>
+        Poslednja promena: 9. 3. 2021. //Uto. 
       </header>
     </div>
   );
