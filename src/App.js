e1c82c78ed32158b347eceda5ad60faf382f670a
CoaPro <srbpro18@gmail.com>
2021-04-17 15:10:29 +0200

    Update 1 | 17.4.2021.

--- src/App.js
+++ src/App.js
@@ -37,9 +37,9 @@ function App() {
         <li>Front-end: React.js</li>
         <li>Back-end: Node.js</li>
         <li>Okvirna tema master rada: </li>
-        <li>JavaScript Veb aplikacije: mrežno programiranje i sajber bezbednost</li> 
+        <li>JavaScript Veb tehnologije: mrežno programiranje i sajber bezbednost</li> 
     </ul>
-        Poslednja promena: 13. 3. 2021. //Sub. 
+        Poslednja promena: 17. 4. 2021. //Sub. 
       </header>
     </div>
   );
